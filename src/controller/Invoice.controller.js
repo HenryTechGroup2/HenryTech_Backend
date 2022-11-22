9881cb241172e65f729b247638a74c8b4a85a3f1
fhekz <fm.aldeco@gmail.com>
2022-11-22 07:56:37 -0300

    Invoice routes

---
+++ src/controller/Invoice.controller.js
@@ -0,0 +1,36 @@
+import Invoice from "../models/Invoice.model.js";
+
+export const getInvoice = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!id) {
+      const invoices = await Invoice.findAll();
+      return res.json(invoices);
+    }
+    const invoice = await Invoice.findByPk(id);
+    return res.json(invoice);
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
+};
+
+export const postInvoice = async (req, res) => {
+  const { invoice_total, invoice_shipping, invoice_amount } = req.body;
+  try {
+    const allInvoice = await Invoice.findAll();
+    if (allInvoice.length === 0) {
+      return res.status(404).json({ msg: "Invoices not found" });
+    }
+    const newInvoice = await Invoice.create({
+      invoice_total,
+      invoice_shipping,
+      invoice_amount,
+    });
+    res.status(201).json({
+      invoice: newInvoice,
+      complete: "Invoice is created succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
+};
