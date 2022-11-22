ddc1be49cf9e21da9bf325cd2589daa2ab797f43
Jonatan <jonatanangel88@gmail.com>
2022-11-22 00:32:58 -0300

    model-invoice-and-car

---
+++ src/controller/Invoice.controller.js
@@ -0,0 +1,20 @@
+import sequelize from '../db.js';
+import { DataTypes } from 'sequelize';
+
+const Invoice = sequelize.define({
+  invoice_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  invoice_amount: {
+    type: DataTypes.DECIMAL(12, 2),
+    allowNull: false,
+  },
+  invoice_shipping: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Invoice;
