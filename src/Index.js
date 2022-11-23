d32447e7601768c02bf650e6c72e56b41f57265e
Nai <62731284+tobiastka@users.noreply.github.com>
2022-11-23 18:32:48 -0300

    Associations
    Se eliminaron las asociaciones del Index y se creo un archivo para las asociaciones, donde se crearon las claves foraneas de las relaciones simples y modelos para las tablas manytomany

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,11 @@
 import app from './App.js';
-import Car from './models/Car.model.js';
-import Invoice from './models/Invoice.mode.js';
+import Order from './models/Order.model.js';
+import Invoice from './models/Invoice.model.js';
 import Product from './models/Product.model.js';
 import User from './models/User.model.js';
 import Review from './models/Review.model.js';
+import Stock from "./models/Stock.model.js"
+import {Product_Order} from './models/Associations.model.js';
 import sequelize from './db.js';
 
 
@@ -11,7 +13,7 @@ const port = 3001;
 
 async function main() {
   try {
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ force: true });
     app.listen(port);
     console.log(`listening on port ${port}`);
   } catch (e) {
@@ -20,24 +22,3 @@ async function main() {
 }
 
 main();
-
-User.hasOne(Car);
-Car.belongsTo(User);
-
-User.belongsToMany(Product, { through: "products_user" });
-Product.belongsToMany(User, { through: "products_user" });
-
-User.hasMany(Invoice);
-Invoice.belongsTo(User);
-
-User.hasMany(Review);
-Review.belongsTo(User);
-
-Product.hasMany(Review);
-Review.belongsTo(Product);
-
-Car.hasMany(Product);
-Product.belongsTo(Car);
-
-Invoice.hasOne(Car);
-Car.belongsTo(Invoice);
