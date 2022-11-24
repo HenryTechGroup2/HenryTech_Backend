f6b7f28682512cd70e9d599ab524ddbeb9548d21
Huaripaz <huaripateypaz@hotmail.com>
2022-11-24 16:23:08 -0300

    stock-routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import routerProduct from './routes/Product.routes.js';
 import routerUser from './routes/User.routes.js';
+import routerStock from './routes/Stock.routes.js';
 import cors from 'cors';
 import morgan from 'morgan';
 
@@ -10,5 +11,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use('/api/product', routerProduct);
 app.use('/api/user', routerUser);
+app.use('/api/stock', routerStock);
 
 export default app;
