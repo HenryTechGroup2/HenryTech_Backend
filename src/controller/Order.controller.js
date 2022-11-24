a9a44b1457fd18eee2f13339e69c68808f3a4436
Huaripaz <huaripateypaz@hotmail.com>
2022-11-24 15:24:42 -0300

    order-routes[WIP]

--- src/controller/Order.controller.js
+++ src/controller/Order.controller.js
@@ -1,90 +1,18 @@
 import Order from '../models/Order.model.js';
 
-export const getAllOrder = async (req, res) => {
-    try {
-        const order = await Order.findAll();
-        return res.status(200).json(order);
-    } catch (error) {
-        return res.status(404).json({ msg: error });
-    };
-};
-
-export const putOrder = async (req, res) => {
-    const { id } = req.params;
-    const {
-        user_email,
-        user_name,
-        user_password,
-        user_phone,
-        user_payment_method,
-        user_shipping_address,
-        user_isAdmin
-    } = req.body;
-    try {
-
-        await User.update(
-            {
-                user_email,
-                user_name,
-                user_password,
-                user_phone,
-                user_payment_method,
-                user_shipping_address,
-                user_isAdmin
-            },
-            {
-                where: {
-                    user_id: Number(id),
-                },
-            }
-        );
-        res.status(200).json({ msg: 'The user was successfully updated' });
-    } catch (error) {
-        res.status(404).json({ msg: error });
-    };
-};
-
 export const postOrder = async (req, res) => {
-    const {
-        user_email,
-        user_name,
-        user_password,
-        user_phone,
-        user_payment_method,
-        user_shipping_address,
-        user_isAdmin
-    } = req.body;
+    const { order_status } = req.body;
 
     try {
         console.log(req.body)
-        const newUser = await User.create({
-            user_email,
-            user_name,
-            user_password,
-            user_phone,
-            user_payment_method,
-            user_shipping_address,
-            user_isAdmin
+        const newOrder = await Order.create({
+            order_status
         });
         res.status(200).json({
-            user: newUser,
-            complete: 'User is created succesfully',
+            order: newOrder,
+            complete: 'Order is created succesfully',
         });
     } catch (error) {
         return res.status(404).json({ msg: error });
     }
-};
-
-export const deleteOrder = async (req, res) => {
-    const { id } = req.params;
-    try {
-        await User.destroy({
-            where: {
-                user_id: id,
-            },
-        });
-        res.status(200).json({ msg: 'The user was deleted successfully' });
-    } catch (error) {
-        res.status(404).json({ msg: error });
-    };
 };
--- src/routes/Order.routes.js
+++ src/routes/Order.routes.js
@@ -1,10 +1,7 @@
 import { Router } from 'express';
-import { getAllOrder, putOrder, deleteOrder, postOrder } from '../controller/Order.controller.js';
+import { postOrder } from '../controller/Order.controller.js';
 
 const router = Router();
-router.get('/', getAllOrder)
-router.put('/:id', putOrder);
-router.delete('/:id', deleteOrder);
 router.post('/', postOrder);
 
 export default router;
