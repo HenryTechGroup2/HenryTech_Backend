096e9d5ff5b96d730fcca665b33d2872e9b0730c
Jonatan <jonatanangel88@gmail.com>
2022-11-27 21:48:52 -0300

    deploy

--- src/controller/Favorite.controller.js
+++ src/controller/Favorite.controller.js
@@ -2,13 +2,14 @@ import User from '../models/User.model.js';
 import Product from '../models/Product.model.js';
 
 export const newFavorite = async (req, res) => {
-    const { user_id, product_id } = req.body;
-    try {
-        const userDB = await User.findByPk(user_id);
-        const productDB = await Product.findByPk(product_id);
-        userDB.addUser_favorite(productDB);
-        res.status(200).json({ msg: "User is created favorite succesfully" })
-    } catch (error) {
-        res.status(404).json({ msg: error });
-    }
-}
+  const { user_id, product_id } = req.body;
+  try {
+    const userDB = await User.findByPk(user_id);
+    const productDB = await Product.findByPk(product_id);
+    userDB.addUser_favorite(productDB);
+    res.status(200).json({ msg: 'User is created favorite succesfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: error });
+  }
+};
