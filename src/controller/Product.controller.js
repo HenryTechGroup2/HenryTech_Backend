89d5824c18fe196f4563b4b0ea79c13725a8c9d3
Nai <62731284+tobiastka@users.noreply.github.com>
2022-11-28 19:38:24 -0300

    Update Product.controller.js
    Se actualizo el controlador de tal forma que cuando se cree un nuevo producto se le asigne correctamente el stock en la tabla de stock, lo mismo para la eliminación de un producto y modificación

--- src/controller/Product.controller.js
+++ src/controller/Product.controller.js
@@ -5,7 +5,7 @@ export const getProducts = async (req, res) => {
   const { name } = req.query;
   try {
     if (!name) {
-      const products = await Product.findAll({include: Stock});
+      const products = await Product.findAll({ include: Stock });
       return res.json(products);
     }
     const product = await Product.findAll({
@@ -23,7 +23,7 @@ export const getProducts = async (req, res) => {
 export const getProduct = async (req, res) => {
   const { id } = req.params;
   try {
-    const product = await Product.findByPk(id, {include: Stock});
+    const product = await Product.findByPk(id, { include: Stock });
     return res.status(200).json(product);
   } catch (error) {
     return res.status(500).json({ msg: error });
@@ -35,23 +35,24 @@ export const postProduct = async (req, res) => {
     product_description,
     product_price,
     product_ofer,
-    product_stock,
     product_rating,
     product_category,
     product_img,
     product_array_img,
+    product_stock,
   } = req.body;
   try {
+    const newStock = await Stock.create({ stock_amount: product_stock });
     const newProduct = await Product.create({
       product_name,
       product_description,
       product_price,
       product_ofer,
-      product_stock,
       product_rating,
       product_category,
       product_img,
       product_array_img,
+      product_stock_id: newStock.stock_id
     });
     res.status(201).json({
       product: newProduct,
@@ -68,32 +69,32 @@ export const putProduct = async (req, res) => {
     product_description,
     product_price,
     product_ofer,
-    product_stock,
     product_rating,
     product_category,
     product_img,
     product_array_img,
+    product_stock,
   } = req.body;
   try {
-    await Product.update(
-      {
-        product_name,
-        product_description,
-        product_price,
-        product_ofer,
-        product_stock,
-        product_rating,
-        product_category,
-        product_img,
-        product_array_img,
-      },
-      {
-        where: {
-          product_id: id,
-        },
-      }
+
+    const productDB = await Product.findOne(
+      { where: { product_id: id } }
     );
-    res.status(204).json({ msg: 'The product was successfully updated' });
+    const productDB_stock = await Stock.findOne(
+      { where: { stock_id: productDB.product_stock_id } }
+    );
+    await productDB.update({
+      product_name,
+      product_description,
+      product_price,
+      product_ofer,
+      product_rating,
+      product_category,
+      product_img,
+      product_array_img,
+    });
+    await productDB_stock.update({ stock_amount: product_stock });
+    res.status(201).json({ msg: 'The product was successfully updated' });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -101,11 +102,18 @@ export const putProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
   try {
-    await Product.destroy({
+
+    const productDB = await Product.findOne({
       where: {
         product_id: id,
-      },
+      }
     });
+    await Stock.destroy({
+      where: {
+        stock_id: productDB.product_stock_id
+      }
+    });
+    await productDB.destroy();
     res.status(200).json({ msg: 'The product was deleted successfully' });
   } catch (error) {
     res.status(500).json({ msg: error });
