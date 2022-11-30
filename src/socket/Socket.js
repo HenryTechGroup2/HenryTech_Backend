0551d28e1970359f33f00fa63e3d00270e879032
Nai <62731284+tobiastka@users.noreply.github.com>
2022-11-30 20:37:19 -0300

    Socket-io
    v1 socket para reviews

---
+++ src/socket/Socket.js
@@ -0,0 +1,19 @@
+import { Server as SocketServer} from 'socket.io';
+import App from '../app.js';
+import http from 'http';
+import Review from '../models/Review.model.js';
+
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+io.on('connection', (socket) => {
+
+    socket.on('@review/create', async (reviewData) => {
+        try {
+            const newReview = await Review.create(...reviewData);
+            io.emit(newReview);
+        } catch (error) {
+            io.emit({err: error});
+        }
+    })
+});
