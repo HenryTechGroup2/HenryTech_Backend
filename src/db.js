43232026039302ece0849bbb45e45aa4ac5a169d
Jonatan <jonatanangel88@gmail.com>
2022-12-12 12:50:51 -0300

    deploy-back

--- src/db.js
+++ src/db.js
@@ -7,8 +7,8 @@ dotenv.config();
 const { PGDATABASE, PGHOST, PGPASSWORD, PGPORT, PGUSER } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
-  { 
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
+  {
     logging: false,
     native: false,
     dialectModule: pg,
--- src/socket/Socket.js
+++ src/socket/Socket.js
@@ -4,11 +4,12 @@ import http from 'http';
 import Review from '../models/Review.model.js';
 import Product from '../models/Product.model.js';
 import { MsgPost, MsgReceived } from '../models/Message.model.js';
-
+const api = 'https://henry-tech-app.vercel.app';
+// const api = 'http://localhost:3000'
 export const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: api,
   },
 });
 
